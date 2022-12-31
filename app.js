33c7975219e66d10b45ed9c74cdb315bff8555c5
luciano <lucianofacundo.12hotmail.com>
2022-12-31 17:00:40 -0300

    nuevo1

--- app.js
+++ app.js
@@ -10,32 +10,7 @@ var hoy = new Date();
 var dia= hoy.getDay();
 var diax="";
 
-switch (dia) {
-  case 0:
-  diax="Domingo"
-  break;
-  case 1:
-  diax="Lunes"
-  break;
-  case 2:
-  diax="Martes"
-  break;
-  case 3:
-  diax="Miercoles"
-  break;
-  case 4:
-  diax="Jueves"
-  break;
-  case 5:
-  diax="Viernes"
-  break;
-  case 6:
-  diax="Sabado"
-  break;
-  default:
-  console.log("ERROR")
 
-}
 
 
   res.render('lista', {mostrar:diax});
