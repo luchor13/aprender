dda315c7f9c36c0e517bb2ebc1315b75f48a486c
luciano <lucianofacundo.12hotmail.com>
2022-12-31 18:11:04 -0300

    ultimos cambios

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const express= require ("express");
-const bodyparser= require ("body-parser");
+const bodyParser= require ("body-parser");
 
 const app= express();
-app.set("view engine", "ejs");
+var items = [];
 
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 var hoy = new Date();
-var dia= hoy.getDay();
-var diax="";
+
+var options = {
+weekday:"long",
+day:"numeric",
+month:"long",
+
+};
 
 
 
 
-  res.render('lista', {mostrar:diax});
+var diax = hoy.toLocaleDateString("en-ES", options);
 
-
-
-
-
+  res.render('lista', {mostrar: diax, tarea1: items});
 
 
 
 
 });
+
+
+app.post("/", function(req, res){
+var item = req.body.tarea;
+items.push(item);
+console.log(item);
+res.redirect("/");
+});
+
+
+
 
 
 app.listen(3000, function(){
